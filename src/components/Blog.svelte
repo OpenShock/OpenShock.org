ffe1385c85671bdcf953fe3fe696372dc8b4618e
Luc ♥ <luc@luc.cat>
2025-08-10 22:57:06 +0200

    feat: migrate site to svelte

--- src/components/Blog.svelte
+++ src/components/Blog.svelte
@@ -1,7 +1,6 @@
----
-import Container from "./Container.astro"
-
----
+<script>
+import Container from './Container.svelte';
+</script>
 
 <div id="blog">
   <Container>
@@ -15,8 +14,7 @@ import Container from "./Container.astro"
       <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
         <div class="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
           <div class="relative overflow-hidden rounded-xl">
-            <img src="https://images.unsplash.com/photo-1661749711934-492cd19a25c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-            alt="art cover" loading="lazy" width="1000" height="667" class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"/>
+            <img src="https://images.unsplash.com/photo-1661749711934-492cd19a25c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWDlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80" alt="art cover" loading="lazy" width="1000" height="667" class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"/>
           </div>
           <div class="mt-6 relative">
             <h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
@@ -29,12 +27,10 @@ import Container from "./Container.astro"
               <span class="text-info dark:text-blue-300">Read more</span>
             </a>
           </div>
-          
         </div>
         <div class="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
           <div class="relative overflow-hidden rounded-xl">
-            <img src="https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-            alt="art cover" loading="lazy" width="1000" height="667" class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"/>
+            <img src="https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWDlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80" alt="art cover" loading="lazy" width="1000" height="667" class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"/>
           </div>
           <div class="mt-6 relative">
             <h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
@@ -47,12 +43,10 @@ import Container from "./Container.astro"
               <span class="text-info dark:text-blue-300">Read more</span>
             </a>
           </div>
-          
         </div>
         <div class="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
           <div class="relative overflow-hidden rounded-xl">
-            <img src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
-            alt="art cover" loading="lazy" width="1000" height="667" class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"/>
+            <img src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWDlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80" alt="art cover" loading="lazy" width="1000" height="667" class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"/>
           </div>
           <div class="mt-6 relative">
             <h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
@@ -65,7 +59,6 @@ import Container from "./Container.astro"
               <span class="text-info dark:text-blue-300">Read more</span>
             </a>
           </div>
-          
         </div>
       </div>
   </Container>
